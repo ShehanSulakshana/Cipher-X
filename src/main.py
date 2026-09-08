8d17ec1cd67d0546f1ad4829c0deb7afc5579d08
ShehanSulakshana <shehansulakshana01@gmail.com>
2026-09-08 22:19:19 +0530

    feat(main) : Added main menu , banner modifications and user selection handler.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+from colorama import Fore, Back, Style
+
 class Main :
+
     def __init__(self):
         self.banner = """
          ██████╗██╗██████╗ ██╗  ██╗███████╗██████╗               ██╗  ██╗
@@ -6,12 +9,52 @@ class Main :
         ██║     ██║██████╔╝███████║█████╗  ██████╔╝    █████╗     ╚███╔╝ 
         ██║     ██║██╔═══╝ ██╔══██║██╔══╝  ██╔══██╗    ╚════╝     ██╔██╗ 
         ╚██████╗██║██║     ██║  ██║███████╗██║  ██║              ██╔╝ ██╗
-         ╚═════╝╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝              ╚═╝  ╚═╝
-                                                                 
+         ╚═════╝╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝              ╚═╝  ╚═╝                                     
         """
+        self.tab_space = "\t"
+
+
+    def display_banner(self):
+        print(f"{Fore.GREEN}{self.banner}{Fore.RESET}")
+        print(f"{self.tab_space *3}{Style.BRIGHT}{Fore.LIGHTWHITE_EX}-{Fore.GREEN} Script By Shehan Sulakshana                  {Style.RESET_ALL}")
+        print(f"{self.tab_space *3}{Style.BRIGHT}-{Fore.GREEN} GITHUB : https://github.com/ShehanSulakshana{Style.RESET_ALL} \n")
+        print(f"\t{Fore.GREEN}{'#' * 75}{Style.RESET_ALL}")
+
+    def display_menu(self):
+        print(f"\n{self.tab_space *3}[1]. Classical Ciphers")
+        print(f"{self.tab_space *3}[2]. Advanced Ciphers")
+
+
+    def handle_options(self):
+        correct_input = False
+        input_value = None
+        while not correct_input:
+
+            try:
+                input_value = int(input(f"\n{self.tab_space}[?] Choice : "))
+
+                if input_value == 1 :
+                    print("Option 1")       #TODO : Implement option method call
+                    correct_input = True
+
+                elif input_value == 2 :
+                    print("Option 2")       #TODO : Implement option method call
+                    correct_input = True
+
+                elif input_value ==3 :
+                    print("Option 3")       #TODO : Implement option method call
+                    correct_input = True
+
+                else:
+                    print(f"{self.tab_space}{Fore.RED}[!] Invalid input {Style.RESET_ALL}")
+            except Exception as e:
+                print(f"{self.tab_space}{Fore.RED}[!] Invalid input {Style.RESET_ALL}")
+
 
     def run(self):
-        print(self.banner)
+        self.display_banner()
+        self.display_menu()
+        self.handle_options()
 
 
 if __name__ == '__main__':
