525100c399097fa05e872f87553a4fbae59bd8ae
ShehanSulakshana <shehansulakshana01@gmail.com>
2026-09-16 20:04:15 +0530

    feat(menu) : Main menu list changed with panned ciphers.

--- src/main.py
+++ src/main.py
@@ -32,7 +32,14 @@ class Main :
         print(f"{self.tab_space *3}[3]. Vigenere Cipher (Poly-alphabetic)")
         print(f"{self.tab_space *3}[4]. Vernam Cipher / One Time Pad (OTP)")
 
-        print(f"\n{self.tab_space * 2}{Fore.BLUE}{Style.BRIGHT}> Symmetric Ciphers {Style.RESET_ALL}")
+        print(f"\n{self.tab_space * 2}{Fore.BLUE}{Style.BRIGHT}> Modern Ciphers {Style.RESET_ALL}")
+        print(f"{self.tab_space *3}[5]. AES (Advanced Encryption Standard)")
+        print(f"{self.tab_space *3}[6]. DES (Data Encryption Standard)")
+        print(f"{self.tab_space *3}[7]. Triple DES (3DES)")
+        print(f"{self.tab_space *3}[8]. RSA (Public-Key Cryptography)")
+
+        # print(f"\n{self.tab_space * 2}{Fore.BLUE}{Style.BRIGHT}> Steganography {Style.RESET_ALL}")
+        # print(f"{self.tab_space *3}[9]. Image Steganography")
 
 
 
